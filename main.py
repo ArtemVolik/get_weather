ee3ea03afe769ec3ccc26b51bc7e6c274fc68d08
artyom <art.volyk@gmail.com>
2020-10-26 01:47:35 +0200

    add readme(installation guide), packages version info)

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import requests
 
-
 cities = ['Лондон', 'Череповец', 'Шереметьево']
-request_params = {'m':'','M':'','lang':'ru'}
+request_params = {'m': '', 'n':'', 'T':'', 'q':'', 'lang': 'ru'}
+
 
 def get_weather(adress, params):
     response = requests.get(adress, params)
     response.raise_for_status()
     print(response.text)
 
-for i in cities:
-    url = f'http://wttr.in/{i}'
-    get_weather(url, request_params)
+
+def main():
+    for city in cities:
+        url = f'http://wttr.in/{city}'
+        get_weather(url, request_params)
+
+
+if __name__ == '__main__':
+    main()
